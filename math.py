579dfdb9bb025a106b3dbcd4975d9f7eda5407b2
SurajNair <suraj_nair1@persistent>
2021-01-25 10:58:55 +0530

    Initial commit for math.py

---
+++ math.py
@@ -0,0 +1,13 @@
+# Perform addition
+def add(x,y):
+    pass
+# Perform subtraction
+def subtract(x,y):
+    pass
+# Perform multiplication
+def multiply(x,y):
+    pass
+    
+# Perform division
+def divide(x,y):
+    pass
